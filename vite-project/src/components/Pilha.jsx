91f39be6650ffcdc9de0614e7dbb037861fa9f7b
Caique Guedes <caiqueluizghpo@gmail.com>
2025-02-03 19:29:04 -0300

    implementa incialmente components

---
+++ vite-project/src/components/Pilha.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+
+
+
+export default function Status() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+  
+    const currentDemon = DEMONS[currentIndex];
+
+    return (
+        <div className="Pilha">
+
+            
+        </div>
+
+
+    )
+}
