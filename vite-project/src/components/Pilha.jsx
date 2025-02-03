8f202dc6603ca49300a98435d5ec97a3c1216e2f
Caique Guedes <caiqueluizghpo@gmail.com>
2025-02-03 19:34:10 -0300

    altera levemente css

--- vite-project/src/components/Pilha.jsx
+++ vite-project/src/components/Pilha.jsx
@@ -1,17 +1,26 @@
 import React, { useState } from "react";
 
+const PILHA = [
 
+    {
+
+
+    }
+]
 
 
 export default function Status() {
     const [currentIndex, setCurrentIndex] = useState(0);
   
-    const currentDemon = DEMONS[currentIndex];
+    
 
     return (
         <div className="Pilha">
+            <div className="nivel">
 
-            
+
+            </div>
+
         </div>
 
 
